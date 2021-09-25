fe32744b2321d3f5de479b658f84ad31f0a5aef8
keroken <kenichi.shinagawa@gmail.com>
2021-09-25 14:04:48 +0900

    add event component

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom';
+import EventComponent from './events/EventComponent';
 import Parent from './props/Parent';
 import GuestList from './state/GuestList';
 import UserSearch from './state/UserSearch';
@@ -10,6 +11,7 @@ const App = () => {
       <Parent />
       <GuestList />
       <UserSearch />
+      <EventComponent />
     </div>
   );
 };
